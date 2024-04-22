9deef988d2ae5f2b9fa7400cccb10332c416a6c5
Sabrun <officialsabrun@gmail.com>
2024-04-22 02:21:58 -0400

    Updated files

---
+++ frontend/src/components/GameGrid.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+
+interface Games {
+  id: number;
+  name: string;
+}
+
+interface GetGames {
+  count: number;
+  results: Games[];
+}
+
+const GameGrid = () => {
+  const [games, setGames] = useState<Games[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<GetGames>("/games")
+      .then((res) => {
+        setGames(res.data.results);
+        setError("");
+      })
+      .catch((err) => {
+        setError(`Error While Fetching: ${err.message}`);
+        setGames([]);
+      });
+  }, []);
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameGrid;
