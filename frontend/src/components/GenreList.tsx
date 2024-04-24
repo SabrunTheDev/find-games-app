051b5e719118de87c0fae1d9276b23d20b5e06ba
Sabrun <officialsabrun@gmail.com>
2024-04-24 00:37:25 -0400

    Updated files

--- frontend/src/components/GenreList.tsx
+++ frontend/src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -22,40 +23,53 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (error) return null;
   if (loading)
     return (
+      <>
+        <Heading fontSize="2xl" marginBottom={3} paddingY="5px">
+          Genres
+        </Heading>
+        <List>
+          {skeletons.map((_, index) => (
+            <ListItem key={index} paddingY="5px">
+              <HStack>
+                <Skeleton boxSize="32px" borderRadius={8} />
+                <SkeletonText width="100px" height="32px" />
+              </HStack>
+            </ListItem>
+          ))}
+        </List>
+      </>
+    );
+
+  return (
+    <>
+      <Heading fontSize="2xl" marginBottom={3} paddingY="5px">
+        Genres
+      </Heading>
       <List>
-        {skeletons.map((_, index) => (
-          <ListItem key={index} paddingY="5px">
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
             <HStack>
-              <Skeleton boxSize="32px" borderRadius={8} />
-              <SkeletonText width="100px" height="32px" />
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCoppedImgUrl(genre.image_background)}
+              />
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              >
+                {genre.name}
+              </Button>
             </HStack>
           </ListItem>
         ))}
       </List>
-    );
-
-  return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCoppedImgUrl(genre.image_background)}
-            />
-            <Button
-              onClick={() => onSelectGenre(genre)}
-              fontSize="lg"
-              variant="link"
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    </>
   );
 };
 
