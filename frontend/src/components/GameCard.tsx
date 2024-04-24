6c509078053cbc7cb3a2a29055ca91602031cd16
Sabrun <officialsabrun@gmail.com>
2024-04-24 00:56:47 -0400

    Updated static genre list

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
 import getCoppedImgUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Games;
@@ -19,7 +20,10 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <Heading fontSize={"2xl"}>
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
